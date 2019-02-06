da569ed666774bba04128bfcf376118dd99c3adb
Harun <harun@eacon.ba>
2019-02-06 15:41:54 +0100

    index page

---
+++ index (2).php
@@ -0,0 +1,47 @@
+<?php 
+include('connection.php');
+
+if(!isset($_SESSION['username'])): ?>
+    Please <a href="login.php">Login</a> or <a href="index.php"> Register </a> 
+<?php 
+header('location: index.php');
+endif ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" media="screen" href="styles.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+</head>
+<body>
+    <form   class='lforma' action="logout.php" method="POST">  
+        <div class="header bg-primary text-white">
+            <h3>Home page</h3>
+        </div>
+        <p> Welcome <?php echo $_SESSION['username']; ?></p>
+    
+        <button class='btn btn-primary btn-lg' type="submit" name="logout">Logout</button>  
+    </form>
+
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    
+</body>
+
+    
+</html>
